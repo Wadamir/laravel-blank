dec934051afcfae036b99ace052082e0b64b6fad
Wadamir <domus159@gmail.com>
2025-02-18 12:55:54 +0500

    settings

---
+++ Dockerfile.php
@@ -0,0 +1,25 @@
+# Using official PHP 8.3 with FPM as a base image
+FROM php:8.3-fpm
+
+# Installing dependencies
+RUN apt-get update && apt-get install -y \
+unzip \
+git \
+curl \
+libpq-dev \
+libzip-dev \
+nodejs \
+npm \
+&& docker-php-ext-install pdo pdo_mysql zip
+
+# Installing Composer
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Installing Laravel Installer
+RUN composer global require laravel/installer
+
+# Adding Composer в PATH
+ENV PATH="/root/.composer/vendor/bin:${PATH}"
+
+# Set working directory
+WORKDIR /var/www
---
+++ www/resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1 class="text-primary">Hello from Vue + Bootstrap!</h1>
+        <button class="btn btn-success">Click me</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+};
+</script>
+
+<style scoped>
+</style>
